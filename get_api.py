17b7e68effbfd0f0abdb722bac2890453c41eb5b
1vanyk1 <ru-vanya@mail.ru>
2020-02-13 17:52:00 +0300

    Part 2

--- get_api.py
+++ get_api.py
@@ -1,2 +1,2 @@
 def get_map(toponym_coodrinates, corners, map_type='sat'):
-    return "http://static-maps.yandex.ru/1.x/?ll=" + toponym_coodrinates + "&spn=" + corners + "&l=" + map_type
+    return "http://static-maps.yandex.ru/1.x/?ll=" + toponym_coodrinates + "&z=" + corners + "&l=" + map_type
--- main.py
+++ main.py
@@ -6,37 +6,41 @@ import requests
 import get_api
 
 
-# geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-# geocoder_params = {
-#     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-#     "geocode": "37.622504,55.753215",
-#     "format": "json"}
-# response = requests.get(geocoder_api_server, params=geocoder_params)
-# json_response = response.json()
-# toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-# toponym_coodrinates = ','.join(toponym["Point"]["pos"].split())
-# corners = [list(map(float, i.split())) for i in toponym['boundedBy']['Envelope'].values()]
-# corners = ','.join([str(corners[1][0] - corners[0][0]), str(corners[1][1] - corners[0][1])])
-# response = None
-cords = [37.622504, 55.753215]
-zoom = 0.01
-map_request = get_api.get_map(",".join(map(str, cords)), str(zoom) + ',' + str(zoom))
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+
+
+def load_image():
+    map_request = get_api.get_map(",".join(map(str, cords)), str(zoom), 'map')
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
+
+cords = [37.622504, 55.753215]
+zoom = 15
+map_file = "map.png"
+load_image()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_PAGEUP:
+                if zoom < 17:
+                    zoom += 1
+                load_image()
+            elif event.key == pygame.K_PAGEDOWN:
+                if zoom > 0:
+                    zoom -= 1
+                load_image()
+    pygame.display.flip()
 pygame.quit()
 os.remove(map_file)
