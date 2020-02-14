3a7b170447ef6b8f852121f2f9fcbda2326aa873
1vanyk1 <57059188+1vanyk1@users.noreply.github.com>
2020-02-14 21:34:09 +0300

    Part 4

--- gui.py
+++ gui.py
@@ -4,6 +4,36 @@ import pygame
 buttons = {}
 
 
+class Button:
+    def __init__(self, x, y, d_x, d_y, text, size, bc_nonact, bc_sel, bc_act,
+                 tc_nonact, tc_sel, tc_act, f, *args, **kwargs):
+        self.active = False
+        self.x = x
+        self.y = y
+        self.d_x = d_x
+        self.d_y = d_y
+        self.text = text
+        self.size = size
+        self.bc_nonact = bc_nonact
+        self.bc_sel = bc_sel
+        self.bc_act = bc_act
+        self.tc_nonact = tc_nonact
+        self.tc_sel = tc_sel
+        self.tc_act = tc_act
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+
+    def get_centre(self):
+        return self.x + int(self.d_x / 2), self.y + int(self.d_y / 2)
+
+    def check_selection(self, pos):
+        return 0 <= pos[0] - self.x <= self.d_x and 0 <= pos[1] - self.y <= self.d_y
+
+    def select(self, b: bool):
+        self.active = b
+
+
 def init():
     global buttons
     pygame.init()
@@ -11,12 +41,11 @@ def init():
 
 
 def draw_rect_const(screen, color, x1, y1, x2, y2, wide=0):
-    pygame.draw(screen, color, (x1, y1, x2 - x1, y2 - y1), wide)
+    pygame.draw.rect(screen, color, (x1, y1, x2 - x1, y2 - y1), wide)
 
 
 def draw_rect(screen, color, x, y, d_x, d_y, wide1=0):
-    print(screen, color, x, y, d_x, d_y, wide1)
-    pygame.draw(screen, color, (x, y, d_x, d_y), wide1)
+    pygame.draw.rect(screen, color, (x, y, d_x, d_y), wide1)
 
 
 def draw_text_at_corner(screen, color, text, x, y, size=50):
@@ -31,13 +60,30 @@ def draw_text_at_centre(screen, color, text, x, y, size=50):
     screen.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
 
-def add_button(id, x, y, d_x, d_y, text, size, bc_nonact, bc_sel, bc_act, tc_nonact, tc_sel, tc_act):
-    buttons[id] = {'active': False, 'x': x, 'y': y, 'd_x': d_x, 'd_y': d_y, 'text': text, 'size': size,
-                   'bc_nonact': bc_nonact, 'bc_sel': bc_sel, 'bc_act': bc_act, 
-                   'tc_nonact': tc_nonact, 'tc_sel': tc_sel, 'tc_act': tc_act}
+def add_button(id, x, y, d_x, d_y, text, size, bc_nonact, bc_sel, bc_act,
+               tc_nonact, tc_sel, tc_act, f, *args, **kwargs):
+    buttons[id] = Button(x, y, d_x, d_y, text, size, bc_nonact, bc_sel, bc_act,
+                         tc_nonact, tc_sel, tc_act, f, *args, **kwargs)
 
 
 def draw_buttons(screen, pos=None):
     for key, item in buttons.items():
-        draw_rect(screen, item['bc_nonact'], item['x'], item['y'], item['d_x'], item['d_y'])
-        draw_text_at_centre(screen, item['tc_nonact'], item['text'], item['x'], item['y'], item['size'])
+        if item.active:
+            draw_rect(screen, item.bc_act, item.x, item.y, item.d_x, item.d_y)
+            draw_text_at_centre(screen, item.tc_act, item.text, *item.get_centre(), item.size)
+        elif item.check_selection(pos):
+            draw_rect(screen, item.bc_sel, item.x, item.y, item.d_x, item.d_y)
+            draw_text_at_centre(screen, item.tc_sel, item.text, *item.get_centre(), item.size)
+        else:
+            draw_rect(screen, item.bc_nonact, item.x, item.y, item.d_x, item.d_y)
+            draw_text_at_centre(screen, item.tc_nonact, item.text, *item.get_centre(), item.size)
+
+
+def button_clicked(pos):
+    for key, item in buttons.items():
+        if item.check_selection(pos):
+            item.f(*item.args, **item.kwargs)
+
+
+def select_button(id, b: bool):
+    buttons[id].select(b)
--- main.py
+++ main.py
@@ -13,6 +13,17 @@ buttons_holding.init()
 gui.init()
 
 
+def change_map_type(type1):
+    global map_type
+    gui.select_button('plan', False)
+    gui.select_button('sputnik', False)
+    gui.select_button('hybrid', False)
+    map_type = type1
+    d = {'map': 'plan', 'sat': 'sputnik', 'sat,skl': 'hybrid'}
+    gui.select_button(d[type1], True)
+    load_image(map_type)
+
+
 def change_zoom(n):
     global zoom
     zoom += n
@@ -20,7 +31,7 @@ def change_zoom(n):
         zoom = 17
     elif zoom < 0:
         zoom = 0
-    load_image()
+    load_image(map_type)
 
 
 def load_image(map_type='map'):
@@ -45,12 +56,13 @@ def move_map(cord, znac):
             cords[1] = -85
     else:
         cords[0] = (cords[0] + 180) % 360 - 180
-    load_image()
+    load_image(map_type)
 
 
 cords = [37.622504, 55.753215]
 zoom = 15
 map_file = "map.png"
+map_type = 'map'
 load_image()
 running = True
 buttons_holding.add_button(pygame.K_UP, move_map, 1, 1)
@@ -59,7 +71,15 @@ buttons_holding.add_button(pygame.K_RIGHT, move_map, 0, 1)
 buttons_holding.add_button(pygame.K_LEFT, move_map, 0, -1)
 buttons_holding.add_button(pygame.K_PAGEUP, change_zoom, 1)
 buttons_holding.add_button(pygame.K_PAGEDOWN, change_zoom, -1)
-gui.add_button(0, 0, 0, 100, 100, 'Tset', 40, (0, 0, 0), (0, 0, 0), (0, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0))
+
+gui.add_button('plan', 0, 0, 70, 30, 'Схема', 25, (127, 127, 127), (191, 191, 191), (255, 255, 255),
+               (0, 0, 0), (0, 0, 0), (0, 0, 0), change_map_type, 'map')
+gui.select_button('plan', True)
+gui.add_button('sputnik', 0, 30, 70, 30, 'Спутник', 25, (127, 127, 127), (191, 191, 191),
+               (255, 255, 255), (0, 0, 0), (0, 0, 0), (0, 0, 0), change_map_type, 'sat')
+gui.add_button('hybrid', 0, 60, 70, 30, 'Гибрид', 25, (127, 127, 127), (191, 191, 191),
+               (255, 255, 255), (0, 0, 0), (0, 0, 0), (0, 0, 0), change_map_type, 'sat,skl')
+mouse_pos = None
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -68,8 +88,12 @@ while running:
             buttons_holding.activate_button(event.key, True)
         if event.type == pygame.KEYUP:
             buttons_holding.activate_button(event.key, False)
+        if event.type == pygame.MOUSEMOTION:
+            mouse_pos = event.pos
+        if event.type == pygame.MOUSEBUTTONUP:
+            gui.button_clicked(event.pos)
     buttons_holding.buttons_actions()
-    gui.draw_buttons(screen)
+    gui.draw_buttons(screen, mouse_pos)
     pygame.display.flip()
 pygame.quit()
 os.remove(map_file)
